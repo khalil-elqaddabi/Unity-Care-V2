3c315f64ba036b3e5428748ee4b0b3fde1659011
khalil <khalilelqaddabi9@gmail.com>
2026-01-05 09:43:06 +0100

    .

--- views/doctors/index.php
+++ views/doctors/index.php
@@ -37,28 +37,36 @@
     </table>
     <br>
 
-    <h2>Ajouter un doctor</h2>
-    <form action="doctors.php?action=create" method="post"> 
-        <label>Prénom:
-            <input type="text" name="first_name" required>
-        </label><br>
-        <label>Nom:
-            <input type="text" name="last_name" required>
-        </label><br>
-        <label>Email:
-            <input type="email" name="email" required>
-        </label><br>
-        <label>Mot de passe:
-            <input type="password" name="password" required>
-        </label><br>
-        <label>Specialisation:
-            <input type="text" name="specialisation">
-        </label><br>
-        <label>Department id:
-            <input type="text" name="department_id">
-        </label><br>
-        <button type="submit">Enregistrer</button>
-    </form>
+   <h2>Ajouter un doctor</h2>
+
+<form action="doctors.php?action=create" method="post">
+    <label>Prénom :
+        <input type="text" name="first_name" required>
+    </label><br>
+
+    <label>Nom :
+        <input type="text" name="last_name" required>
+    </label><br>
+
+    <label>Email :
+        <input type="email" name="email" required>
+    </label><br>
+
+    <label>Mot de passe :
+        <input type="password" name="password" required>
+    </label><br>
+
+    <label>Spécialisation :
+        <input type="text" name="specialisation">
+    </label><br>
+
+    <label>Department id :
+        <input type="text" name="department_id">
+    </label><br>
+
+    <button type="submit">Enregistrer</button>
+</form>
+
 </body>
 
 </html>
